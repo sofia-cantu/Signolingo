26f9ef58d194e4be3b892733031246da44849cbe
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-02-29 11:23:59 -0600

    todas las pages
    ya se puede iniciar el diseño de estas

---
+++ WebFront/src/pages/Datos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Datos = () => {
+    return (
+        <div>
+            <h1>Donde se muestran los Datos</h1>
+        </div>
+      )
+}
+
+export default Datos
---
+++ WebFront/src/pages/MainTutorial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainTutorial = () => {
+    return (
+        <div>
+            <h1>Pagina Principal</h1>
+        </div>
+      )
+}
+
+export default MainTutorial;
